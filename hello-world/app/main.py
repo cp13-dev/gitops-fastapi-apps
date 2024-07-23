35d21364ae8058d40851d1403a6dd43023ff969f
Chris Papadakis <chrispapadakis@Athinas-MacBook-Air.local>
2024-07-23 17:20:45 +0300

    started adding security features

--- hello-world/app/main.py
+++ hello-world/app/main.py
@@ -1,6 +1,14 @@
+import os
 import json
-from fastapi import FastAPI
+import secrets
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.status import HTTP_401_UNAUTHORIZED
 from pydantic import Field, BaseModel
+
+
+app = FastAPI()
+security = HTTPBasic()
 
 class UserRequest(BaseModel):
     user_input: str = Field(
@@ -8,13 +16,21 @@ class UserRequest(BaseModel):
         description="Here goes the user input"
     )
 
-app = FastAPI()
+def authenticate(credentias: HTTPBasicCredentials=Depends(security)):
+    correct_username = secrets.compare_digest(credentias.username, os.getenv("BASIC_AUTH_USERNAME"))
+    correct_password = secrets.compare_digest(credentias.password, os.getenv("BASIC_AUTH_PASSWORD"))
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"}
+        )
 
 @app.get("/")
 def root():
     return {"Hello SWISS user!!"}
 
-@app.post("/stars")
+@app.post("/stars", dependencies=[Depends(authenticate)])
 def stars(request: UserRequest):
     data = json.loads(request.json())
     user_input = data.get("user_input", "")
